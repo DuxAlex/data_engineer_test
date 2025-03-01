b047356f6099436c9f78251b7eae4c7c4acf1dbc
DuxAlex <alexkrypto.ti@gmail.com>
2025-03-01 19:17:55 -0300

    alterações feita no código optmize e no verificador de otimização no diretório help. falta verificar testar e subir executar o projeto do zero pra validar alterações e depois disso documentar e fazer as querys de consulta.

--- ETL/4_optimize/1_optimize_tabelas.sql
+++ ETL/4_optimize/1_optimize_tabelas.sql
@@ -14,44 +14,41 @@ CREATE INDEX IF NOT EXISTS idx_data_transacao ON transacoes(data_transacao);
 CREATE INDEX IF NOT EXISTS idx_id_cliente_total ON total_por_cliente(id_cliente);
 
 -- Verificação de particionamento para a tabela transacoes
-DO $
-BEGIN
-    -- Verificar se a tabela 'transacoes' já está particionada
-    IF NOT EXISTS (SELECT 1 FROM pg_inherits WHERE inhparent = 'transacoes'::regclass) THEN
-        
-        -- Criar a tabela particionada 'transacoes_particionada' com chave primária incluindo a coluna de particionamento
-        EXECUTE 'CREATE TABLE transacoes_particionada (
-            id SERIAL PRIMARY KEY,
-            id_cliente INT,
-            id_produto INT,
-            quantidade INT,
-            data_transacao DATE
-        ) PARTITION BY RANGE (data_transacao)';
-        
-        -- Criar partições para a tabela 'transacoes_particionada' (de 2020 a 2024)
-        EXECUTE 'CREATE TABLE transacoes_2020 PARTITION OF transacoes_particionada 
-                 FOR VALUES FROM (''2020-01-01'') TO (''2020-12-31'')';
-        EXECUTE 'CREATE TABLE transacoes_2021 PARTITION OF transacoes_particionada 
-                 FOR VALUES FROM (''2021-01-01'') TO (''2021-12-31'')';
-        EXECUTE 'CREATE TABLE transacoes_2022 PARTITION OF transacoes_particionada 
-                 FOR VALUES FROM (''2022-01-01'') TO (''2022-12-31'')';
-        EXECUTE 'CREATE TABLE transacoes_2023 PARTITION OF transacoes_particionada 
-                 FOR VALUES FROM (''2023-01-01'') TO (''2023-12-31'')';
-        EXECUTE 'CREATE TABLE transacoes_2024 PARTITION OF transacoes_particionada 
-                 FOR VALUES FROM (''2024-01-01'') TO (''2024-12-31'')';
-        
-        -- Transferir os dados da tabela original 'transacoes' para a tabela particionada
-        EXECUTE 'INSERT INTO transacoes_particionada (id_cliente, id_produto, quantidade, data_transacao)
-                 SELECT id_cliente, id_produto, quantidade, data_transacao FROM transacoes';
-        
-        -- Remover a tabela original 'transacoes'
-        EXECUTE 'DROP TABLE IF EXISTS transacoes';
+SELECT COUNT(*) 
+FROM pg_inherits 
+WHERE inhparent = 'transacoes'::regclass;
 
-        -- Renomear a tabela particionada para 'transacoes'
-        EXECUTE 'ALTER TABLE transacoes_particionada RENAME TO transacoes';
-        
-        RAISE NOTICE ''A tabela ''transacoes'' foi particionada com sucesso.'';
-    ELSE
-        RAISE NOTICE ''A tabela ''transacoes'' já está particionada.'';
-    END IF;
-END $;
+-- Se a tabela transacoes não estiver particionada, particionar a tabela
+-- (Execute o seguinte bloco apenas se a contagem for 0)
+
+-- Criar a tabela particionada 'transacoes_particionada' com chave primária incluindo a coluna de particionamento
+CREATE TABLE transacoes_particionada (
+    id SERIAL,
+    id_cliente INT,
+    id_produto INT,
+    quantidade INT,
+    data_transacao DATE,
+    PRIMARY KEY (id, data_transacao)  -- Incluindo data_transacao na chave primária
+) PARTITION BY RANGE (data_transacao);
+
+-- Criar partições para a tabela 'transacoes_particionada' (de 2020 a 2024)
+CREATE TABLE transacoes_2020 PARTITION OF transacoes_particionada 
+    FOR VALUES FROM ('2020-01-01') TO ('2021-01-01');  -- Limite superior exclusivo
+CREATE TABLE transacoes_2021 PARTITION OF transacoes_particionada 
+    FOR VALUES FROM ('2021-01-01') TO ('2022-01-01');  -- Limite superior exclusivo
+CREATE TABLE transacoes_2022 PARTITION OF transacoes_particionada 
+    FOR VALUES FROM ('2022-01-01') TO ('2023-01-01');  -- Limite superior exclusivo
+CREATE TABLE transacoes_2023 PARTITION OF transacoes_particionada 
+    FOR VALUES FROM ('2023-01-01') TO ('2024-01-01');  -- Limite superior exclusivo
+CREATE TABLE transacoes_2024 PARTITION OF transacoes_particionada 
+    FOR VALUES FROM ('2024-01-01') TO ('2025-01-01');  -- Limite superior exclusivo
+
+-- Transferir os dados da tabela original 'transacoes' para a tabela particionada
+INSERT INTO transacoes_particionada (id_cliente, id_produto, quantidade, data_transacao)
+SELECT id_cliente, id_produto, quantidade, data_transacao::date FROM transacoes;
+
+-- Remover a tabela original 'transacoes'
+DROP TABLE IF EXISTS transacoes;
+
+-- Renomear a tabela particionada para 'transacoes'
+ALTER TABLE transacoes_particionada RENAME TO transacoes;
